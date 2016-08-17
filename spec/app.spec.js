dd3e2f34a8ee4d279dafdc0d20670072c1e9440f
Rencheng Chua <rench00@gmail.com>
2016-08-17 11:21:20 +0800

    add travis

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,2 +1,25 @@
 var  app = require('../app');
 var supertest = require("supertest");
+
+describe("Express Server API", function () {
+  describe("should return users json on get /users", function () {
+    it("returns status code 200", function (done) {
+      supertest(app)
+        .get('/movies')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /json/)
+        .expect(200,done);
+    });
+
+  });
+
+  it('returns status 404 when name is not found', function(done) {
+   console.log("in 404 test");
+   supertest(app)
+     .get('/users/junius')
+     .set('Accept', 'application/json')
+     .expect('Content-Type', /json/)
+     .expect(404);
+     done();
+ });
+});
